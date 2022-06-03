bf70b702c3bdc710c52811a3827fe6c80b65b536
Justin <justinanthony.primary@gmail.com>
2022-06-03 21:32:06 +0800

    [feat] new api route for srk and sfuel cmc data

--- src/api/jobs/cmc.js
+++ src/api/jobs/cmc.js
@@ -5,10 +5,27 @@ const router = express.Router();
 
 const axios = require('axios');
 
-router.get('/', async (req, res) => {
+router.get('/srk', async (req, res) => {
   await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest', {
     params: {
       symbol: 'SRK'
+    },
+    headers: {
+      'X-CMC_PRO_API_KEY': process.env.CMC_PRO_API_KEY,
+    }
+  })
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+router.get('/sfuel', async (req, res) => {
+  await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest', {
+    params: {
+      symbol: 'SFUEL'
     },
     headers: {
       'X-CMC_PRO_API_KEY': process.env.CMC_PRO_API_KEY,
